0e91454d94e9e49bbedf4383034e1400c574b522
s3496h <s3496h@gmail.com>
2024-10-08 21:32:14 +0900

    질문 목록 만들기

--- src/test/java/com/example/basic/SpringBasicApplicationTests.java
+++ src/test/java/com/example/basic/SpringBasicApplicationTests.java
@@ -1,11 +1,16 @@
 package com.example.basic;
 
+import com.example.basic.answer.Answer;
+import com.example.basic.answer.AnswerRepository;
 import com.example.basic.qusetion.Question;
 import com.example.basic.qusetion.QuestionRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,15 +19,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
 class SpringBasicApplicationTests {
 
-	@Autowired
-	private QuestionRepository questionRepository;
+    @Autowired
+    private AnswerRepository answerRepository;
+    @Autowired
+    private QuestionRepository questionRepository;
 
-	@Test
-	void testJpa() {
-    Optional<Question> oq = this.questionRepository.findById(1);
-	assertTrue(oq.isPresent());
-	Question q = oq.get();
-	q.setSubject("수정된 제목");
-	this.questionRepository.save(q);
-	}
+    @Test
+    @Transactional
+    void testJpa() {
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        List<Answer> answerList = q.getAnswerList();
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+
+
+    }
 }
+
