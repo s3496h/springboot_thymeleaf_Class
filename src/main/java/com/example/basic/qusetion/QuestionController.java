dcea50482ea9882860f67f9ae1426070d4d37302
s3496h <s3496h@gmail.com>
2024-10-24 21:49:26 +0900

    질문 서비스와 컨트롤러 수정하기

--- src/main/java/com/example/basic/qusetion/QuestionController.java
+++ src/main/java/com/example/basic/qusetion/QuestionController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -68,5 +69,29 @@ public class QuestionController {
     questionForm.setSubject(question.getSubject());
     questionForm.setContent(question.getContent());
     return "question_form";
+    }
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String questionModify(@Valid QuestionForm questionForm, BindingResult bindingResult,
+                                 Principal principal, @PathVariable("id") Integer id) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        Question question = this.questionService.getQuestion(id);
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
+        return String.format("redirect:/question/detail/%s", id);
+    }
+    @PreAuthorize("isAuthenticated()")
+     @GetMapping("/delete/{id}")
+     public String questionDelete(Principal principal, @PathVariable("id")Integer id){
+    Question question = this.questionService.getQuestion(id);
+    if(!question.getAuthor().getUsername().equals(principal.getName())){
+    throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"삭제 권한이 없습니다.");
+    }
+    this.questionService.delete(question);
+    return "redirect:/";
     }
 }
